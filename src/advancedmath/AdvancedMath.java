34796f3a1a9c8f27710a0d9c82ff5d203a73f217
Fahd Ananta <f.ananta@gmail.com>
2011-11-25 00:24:34 -0500

    implemented atanh; changed functions to accept double

--- src/advancedmath/AdvancedMath.java
+++ src/advancedmath/AdvancedMath.java
@@ -19,36 +19,43 @@ public class AdvancedMath {
         return (int) Math.rint(Math.abs(hi - low) * Math.random()) + low;
     }
 
-    public static double cosh(int x) {
+    public static double cosh(double x) {
         return (0.5 * (Math.exp(-1 * x) + Math.exp(x)));
     }
 
-    public static double sinh(int x) {
+    public static double sinh(double x) {
         return (0.5 * ((-1) * Math.exp(-1 * x) + Math.exp(x)));
     }
 
-    public static double tanh(int x) {
+    public static double tanh(double x) {
         return (sinh(2 * x)) / (cosh(2 * x) + 1);
     }
 
-    public static double acosh(int x) throws ComplexNumberException {
+    public static double acosh(double x) throws ComplexNumberException {
         if (((-1 + x) < 0) || ((1 + x) < 0)) {
             throw new ComplexNumberException();
         }
         return Math.log(x + (Math.sqrt(-1 + x) * Math.sqrt(1 + x)));
     }
 
-    public static double asinh(int x) throws ComplexNumberException {
-        double innerResult = x + Math.sqrt(1 + Math.pow((double) x, 2.0));
-        if (innerResult < 0) {
+    public static double asinh(double x) throws ComplexNumberException {
+        double innerResult = x + Math.sqrt(1 + Math.pow(x, 2.0));
+        if (innerResult <= 0) {
             throw new ComplexNumberException();
         }
         return Math.log(innerResult);
     }
 
+    public static double atanh(double x) throws ComplexNumberException {
+        if ((x <= -1) || (x >= 1)) {
+            throw new ComplexNumberException();
+        }
+        return (0.5 * (Math.log(1 + x) - Math.log(1 - x)));
+    }
+
     public static void main(String[] args) {
         try {
-            System.out.println(asinh(0));
+            System.out.println(atanh(0.5));
         } catch (ComplexNumberException e) {
             System.err.println(e.getMessage());
         }
